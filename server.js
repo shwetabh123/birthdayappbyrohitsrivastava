5afd7bb96bbf1096a3ccb4d7e49c1564217035c6
shwetabh <shwetabh.srivastava@gmail.com>
2020-04-09 18:34:59 +0530

    awwjjjfff99w

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ var app =express();
 
 app.use(bodyparser.json());
 
-//app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/uploads'));
 
 //app.use(express.static(__dirname));
 
